cf99ac143ad59d99b9944ef6e5a1897ce85e962f
abhiram11 <abhiramsatpute@gmail.com>
2021-09-01 13:26:13 +0530

    adding full demo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense } from "react";
+import "./App.css";
+// import Demo from "./components/Demo";
+const Demo = lazy(() => import("./components/Demo"));
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>This is normal header</h1>
+        <Suspense fallback={<>Loading...!!!!</>}>
+          <Demo />
+        </Suspense>
       </header>
     </div>
   );
---
+++ src/components/Demo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Demo() {
+  return <div>Hi There I am now loaded!!!!!</div>;
+}
+
+export default Demo;
